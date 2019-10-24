f60b6460995afb94d01eb246037420d67028fda6
trevor clements <trevortheamazing@gmail.com>
2019-10-24 13:22:02 -0500

    init commit.  user stories updated.  Superhero class and db model complete.

---
+++ SuperheroesWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuperheroesWebApp.MvcApplication" Language="C#" %>
---
+++ SuperheroesWebApp/Models/Superhero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuperheroesWebApp.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string alterEgoName { get; set; }
+        public string primarySuperheroAbility { get; set; }
+        public string secondarySuperheroAbility { get; set; }
+        public string catchphrase { get; set; }
+    }
+}
