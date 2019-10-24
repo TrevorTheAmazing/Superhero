1afb11fb346ee47b9778b054adc8e8c77e951614
trevor clements <trevortheamazing@gmail.com>
2019-10-24 11:01:58 -0500

    init commit.

---
+++ CesiumDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CesiumDemo.Startup))]
+namespace CesiumDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
